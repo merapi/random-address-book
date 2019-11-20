f16f0e146e7046a3a654dc5f5e48235422834beb
merapi <marcin@webskill.pl>
2019-11-20 13:54:59 +0100

    Flags for settings

---
+++ src/assets/flags/index.tsx
@@ -0,0 +1,4 @@
+export { ReactComponent as CH } from './CH.svg'
+export { ReactComponent as ES } from './ES.svg'
+export { ReactComponent as FR } from './FR.svg'
+export { ReactComponent as GB } from './GB.svg'
